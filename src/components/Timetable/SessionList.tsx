7764a153b1634263d1849e3901243991f22d01c1
Mwero Abdalla <mwerothewebmaker@gmail.com>
2024-09-05 13:40:15 +0300

    fix : bug-fix in /api

--- src/components/Timetable/SessionList.tsx
+++ src/components/Timetable/SessionList.tsx
@@ -26,18 +26,18 @@ const SessionList: React.FC<SessionListProps> = ({ sessions }) => {
     forms = { ...forms, [form]: sn };
   }
   return (
-    <div className="w-full ">
+    <div className="w-full " >
       {Object.values(forms).map((session: any, index) => (
-        <div className={cn(`border-t-2 border-black w-full h-16  bg-slate-100`,`${index == 0? 'border-t-0':''}`) }>
+        <div key={index} className={cn(`border-t-2 border-black w-full h-16  bg-slate-100`,`${index == 0? 'border-t-0':''}`) }>
           {session ? (
             <Session
-              key={index}
+              
               teacher={session?.teachers.map((tr: any) => tr.code).join("/")}
               className={session?.className}
               name={session?.name}
             />
           ) : (
-            <div key={index} className="flex items-center justify-center">
+            <div  className="h-full -w-full flex items-center justify-center">
               x
             </div>
           )}
