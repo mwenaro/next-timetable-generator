3555a6e6d581603ca8c4d8e46fedf79d749226c3
Mwero Abdalla <mwerothewebmaker@gmail.com>
2024-09-27 15:12:38 +0300

    chore: renamed mpesa to payment

--- src/app/api/payment/stk/route.ts
+++ src/app/api/payment/stk/route.ts
@@ -3,10 +3,15 @@ import { NextRequest, NextResponse } from "next/server";
 
 // path to handle stkpush
 export async function POST(req: NextRequest) {
-  
+  const mpesaUrl = req.url.split("/stk")[0];
+  const confirmUrl = `${mpesaUrl}/stk/callback`;
   try {
     const { phone, amount } = await req.json();
-    const data = await safaricomDarajaApi.intiateC2bStkPush(phone, amount);
+    const data = await safaricomDarajaApi.intiateC2bStkPush(
+      phone,
+      amount,
+      confirmUrl
+    );
 
     return NextResponse.json({ sucess: true, data });
   } catch (error: any) {
